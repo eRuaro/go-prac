f58d1e036915a65dce7654da3ad9c4e97308ea46
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-08-18 16:55:50 +0800

    Added code for shorthand slices

--- data-structures/map_slices.go
+++ data-structures/map_slices.go
@@ -24,4 +24,7 @@ func main() {
 	numbers := []int{5, 8}
 	numbers = append(numbers, 11)
 	log.Println(numbers)
+
+	// print first two numbers
+	log.Println(numbers[0:2]) // [numbers[0], numbers[2])
 }
