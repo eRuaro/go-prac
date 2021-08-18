159ce97d756fa352de1173182de0fef7945c4ff4
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-08-18 16:26:27 +0800

    Added code for receivers

--- types-structs/types_structs.go
+++ types-structs/types_structs.go
@@ -11,10 +11,19 @@ type Person struct {
 	Age  int
 }
 
+type withFunction struct {
+	FirstName string
+}
+
+// receiver allows to use functions for structs
+func (m *withFunction) printFirstName() string {
+	return m.FirstName
+}
+
 func main() {
-	word := "Hey"
-	log.Println("Word is,", word)
-	saySomething(word)
+	//word := "Hey"
+	//log.Println("Word is,", word)
+	//saySomething(word)
 
 	neil := Person{
 		Name: "Neil",
@@ -24,9 +33,17 @@ func main() {
 	log.Println(neil.Name)
 	log.Println(neil.Age)
 	log.Println(neil)
+
+	myVar := withFunction{
+		FirstName: "My Var",
+	}
+
+	log.Println("My Var is set to:", myVar.FirstName)
+	// accessing a member using a function
+	log.Println(myVar.printFirstName())
 }
 
 // visible to only this package
-func saySomething(word string) {
-	log.Println("Saying:", word)
-}
+//func saySomething(word string) {
+//	log.Println("Saying:", word)
+//}
