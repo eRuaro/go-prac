82c472393c76a4075254ecd02b214c506618602a
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-08-18 13:55:18 +0800

    pointers with functions

--- pointers.go
+++ pointers.go
@@ -1,19 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"log"
 )
 
 func main() {
 	var a int = 1
 	// takes in memory address of a
 	var b *int = &a
-	fmt.Println(a, *b)
-	fmt.Println(b, &a)
+	log.Println(a, *b)
+	log.Println(b, &a)
 
 	// change value of a
 	a = 2
 	// b is also changed due to having same memory address of a
-	fmt.Println(a, *b)
-	fmt.Println(b, &a)
+	log.Println(a, *b)
+	log.Println(b, &a)
+
+	point(&a, b)
+}
+
+func point(x *int, y *int) {
+	log.Println(x, y)
 }
