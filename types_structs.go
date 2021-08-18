ed9d7c7b4f9c40f25a6a08fa91d853f346ae851d
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-08-18 14:08:20 +0800

    used structs

--- types_structs.go
+++ types_structs.go
@@ -4,10 +4,22 @@ import(
 	"log"
 )
 
+type Person struct {
+	Name string
+	Age int
+}
+
 func main() {
 	word := "Hey"
 	log.Println("Word is,", word)	
 	saySomething(word)
+
+	neil := Person{
+		Name: "Neil",
+		Age:  17,
+	}
+
+	log.Println(neil)
 }
 
 func saySomething(word string) {
