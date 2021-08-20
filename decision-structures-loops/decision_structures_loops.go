fb1c5dcc965d8997efd799f3da70a387ea032b9c
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-08-21 07:57:31 +0800

    Added code for if statements, looping

--- decision-structures-loops/decision_structures_loops.go
+++ decision-structures-loops/decision_structures_loops.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
 	for number := 0; number < 20; number++ {
@@ -13,5 +16,20 @@ func main() {
 		} else {
 			fmt.Println("Number:", number, " is less than 20")
 		}
+	}
+
+	mySlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Println("Ranging over Slice")
+	for _, x := range mySlice {
+		log.Println(x)
+	}
+
+	myMap := make(map[string]int)
+	myMap["Dog"] = 5
+	myMap["Cat"] = 10
+	myMap["Fish"] = 15
+	fmt.Println("Ranging over Map")
+	for i, x := range myMap {
+		log.Println(i, x)
 	}
 }
